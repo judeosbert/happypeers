bedfb651e1e23c19decc7377c47b2817370cccd0
Jude Osbert K <judeosby@gmail.com>
2023-07-10 12:36:21 +0530

    add clarity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,12 @@
 import { initAuth } from '@/initAuth'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { clarity } from 'clarity-js'
-import { useEffect } from 'react'
 
 initAuth()
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(()=>{
-    let config = {
-      projectId:"hwzlun3yd8"
-    }
-    clarity.start(config)
-  },[])
   return <Component {...pageProps} />
 }
 
