e8e152b25c88e843dfd083bfbd1e2ba8d0aac220
Jude Osbert K <judeosby@gmail.com>
2023-07-12 21:27:49 +0530

    Add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,23 @@
 import { initAuth } from '@/initAuth'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 initAuth()
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-XF3MP6YJVT" />
+    <Script id="google-analytics">{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-XF3MP6YJVT');
+`}
+    </Script>
+    <Component {...pageProps} /></>
 }
 
